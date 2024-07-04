737b755bd09c38682f53a36e9507465a8f04f080
nagumallivinay123 <nagumallivinay123@gmail.com>
2024-07-04 11:34:04 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import Logo from '../assets/logo.svg';
+import LandingImg from '../assets/main.svg';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+export default function Home() {
+  return (
+    <main>
+      <header className='max-w-6xl mx-auto px-4 sm:px-8 py-6 '>
+        <Image src={Logo} alt='logo' />
+      </header>
+      <section className='max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center'>
+        <div>
+          <h1 className='capitalize text-4xl md:text-7xl font-bold'>
+            job <span className='text-primary'>tracking</span> app
+          </h1>
+          <p className='leading-loose max-w-md mt-4 '>
+          Welcome to Jobify, your ultimate job tracking solution! Designed to streamline your job search, Jobify allows you to effortlessly organize and monitor your job applications in one convenient place. Keep track of every detail—from application status to interview dates—with our user-friendly interface   
+          </p>
+          <Button asChild className='mt-4'>
+            <Link href='/add-job'>Get Started</Link>
+          </Button>
+        </div>
+        <Image src={LandingImg} alt='landing' className='hidden lg:block ' />
+      </section>
+    </main>
+  );
+}
