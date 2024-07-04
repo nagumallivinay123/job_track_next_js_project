881af45c15c7515d048a864322f1046ecb67a2b7
nagumallivinay123 <nagumallivinay123@gmail.com>
2024-07-04 11:56:44 +0530

    change name

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Providers from './providers';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Jobify Dev',
+  title: 'JobTrack',
   description: 'Job application tracking system for job hunters',
 };
 
